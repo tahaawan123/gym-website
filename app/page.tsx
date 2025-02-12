1f0612068578849119f0336ee5cfe43e95a1bdb3
MUHAMMAD TAHA <tahaawan598@gmail.com>
2025-02-12 22:12:27 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import DataPage from "./Data/page";
 import AboutPage from "./About/page";
 import ReviewsPage from "./GymReviews/page";
 import FormPage from "./Form/page";
-import Footer from "./components/Footer";
+import Footer from "../components/Footer";
 
 function Homepage() {
   return (
